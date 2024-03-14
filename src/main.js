6be6d858a17a3e268b30f0eb2a5fb127ec82015e
Aida Wu <ada10050616@gmail.com>
2024-03-14 13:25:35 +0800

    new project

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { library } from '@fortawesome/fontawesome-svg-core'
+// fas 是所有的 solid icon
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-createApp(App).use(router).mount('#app')
+library.add(fas)
+
+const app = createApp(App)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
+app.config.productionTip = false
+
+app.use(router)
+app.mount('#app')
