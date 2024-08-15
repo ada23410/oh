e6b2b920ca8fefb2942d7c684dce8081e845b932
aida wu <ada10050616@gmail.com>
2024-08-15 21:02:31 +0800

    新增動態

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -75,7 +75,7 @@
             <p>專門銷售各種飲料（如手工啤酒、精釀咖啡、茶飲等）和醃漬物（如醃黃瓜pickles、橄欖black olives、泡菜kimchi等）。這些產品既可獨立享用，也可作為搭配美食的佳品。</p>
           </div>
           <router-link class="call-to-action" to="/about">
-            查看資訊
+            <span>查看資訊</span>
             <font-awesome-icon class="bars" icon="fas fa-arrow-right"/>
           </router-link>
         </div>
